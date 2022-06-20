b1439f05ac03d655b57d9129c33a2a98a061a81b
hassanesidiammi <h.sidiammi@gmail.com>
2022-06-20 03:00:38 +0100

    User, add user command.

---
+++ src/Command/AddUserCommand.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Command;
+
+use ApiPlatform\Core\Exception\InvalidArgumentException;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+use function Symfony\Component\String\u;
+
+
+class AddUserCommand extends Command
+{
+    protected static $defaultName = 'app:add-user';
+
+    private $io;
+
+    private $entityManager;
+    private $passwordHasher;
+    private $users;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher, UserRepository $users)
+    {
+        parent::__construct();
+
+        $this->entityManager = $em;
+        $this->passwordHasher = $passwordHasher;
+        $this->users = $users;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Creates user and stores it in the database')
+            ->setHelp($this->getCommandHelp())
+            ->addArgument('username', InputArgument::OPTIONAL, 'The username of the new user')
+            ->addArgument('email', InputArgument::OPTIONAL, 'The email of the new user')
+            ->addArgument('password', InputArgument::OPTIONAL, 'The plain password of the new user')
+            ->addOption('admin', null, InputOption::VALUE_NONE, 'If set, the user is created as an administrator')
+        ;
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        if (null !== $input->getArgument('username') && null !== $input->getArgument('password') && null !== $input->getArgument('email')) {
+            return;
+        }
+
+        $this->io->title('Add User Command Interactive Wizard');
+        $this->io->text([
+            'If you prefer to not use this interactive wizard, provide the',
+            'arguments required by this command as follows:',
+            '',
+            ' $ php bin/console app:add-user username password email@example.com',
+            '',
+            'Now we\'ll ask you for the value of all the missing command arguments.',
+        ]);
+
+        $username = $input->getArgument('username');
+        if (null !== $username) {
+            $this->io->text(' > <info>Username</info>: '.$username);
+        } else {
+            $username = $this->io->ask('Username', 'test');
+            $input->setArgument('username', $username);
+        }
+
+        // Ask for the email if it's not defined
+        $email = $input->getArgument('email');
+        if (null !== $email) {
+            $this->io->text(' > <info>Email</info>: '.$email);
+        } else {
+            $email = $this->io->ask('Email', 'test@foo.com');
+            $input->setArgument('email', $email);
+        }
+  
+        // Ask for the password if it's not defined
+        $password = $input->getArgument('password');
+        if (null !== $password) {
+            $this->io->text(' > <info>Password</info>: '.u('*')->repeat(u($password)->length()));
+        } else {
+            $password = $this->io->askHidden('Password (your type will be hidden)');
+            $input->setArgument('password', $password);
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $stopwatch = new Stopwatch();
+        $stopwatch->start('add-user-command');
+
+        $username = $input->getArgument('username');
+        $plainPassword = $input->getArgument('password');
+        $email = $input->getArgument('email');
+        $isAdmin = $input->getOption('admin');
+
+        $this->validateUserData($username, $plainPassword, $email);
+
+        // create the user and hash its password
+        $user = new User();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setRoles([$isAdmin ? 'ROLE_ADMIN' : 'ROLE_USER']);
+
+        // See https://symfony.com/doc/current/security.html#c-encoding-passwords
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $this->io->success(sprintf('%s was successfully created: %s (%s)', $isAdmin ? 'Administrator user' : 'User', $user->getUsername(), $user->getEmail()));
+
+        $event = $stopwatch->stop('add-user-command');
+        if ($output->isVerbose()) {
+            $this->io->comment(sprintf('New user database id: %d / Elapsed time: %.2f ms / Consumed memory: %.2f MB', $user->getId(), $event->getDuration(), $event->getMemory() / (1024 ** 2)));
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function validateUserData($username, $plainPassword, $email): void
+    {
+      // first check if a user with the same username already exists.
+      $existingUser = $this->users->findOneBy(['username' => $username]);
+
+      if (null !== $existingUser) {
+          throw new RuntimeException(sprintf('There is already a user registered with the "%s" username.', $username));
+      }
+
+      // validate Password
+      if (empty($plainPassword)) {
+          throw new InvalidArgumentException('The password can not be empty.');
+      }
+
+      if (u($plainPassword)->trim()->length() < 3) {
+          throw new InvalidArgumentException('The password must be at least 3 characters long.');
+      }
+
+      // validate Email
+      if (empty($email)) {
+        throw new InvalidArgumentException('The email can not be empty.');
+      }
+
+      if (null === u($email)->indexOf('@')) {
+        throw new InvalidArgumentException('The email should look like a real email.');
+      }
+
+      // check if a user with the same email already exists.
+      $existingEmail = $this->users->findOneBy(['email' => $email]);
+
+      if (null !== $existingEmail) {
+          throw new RuntimeException(sprintf('There is already a user registered with the "%s" email.', $email));
+      }
+    }
+
+    private function getCommandHelp(): string
+    {
+        return <<<'HELP'
+The <info>%command.name%</info> command creates new users and saves them in the database:
+
+  <info>php %command.full_name%</info> <comment>username password email</comment>
+
+By default the command creates regular users. To create administrator users,
+add the <comment>--admin</comment> option:
+
+  <info>php %command.full_name%</info> username password email <comment>--admin</comment>
+
+If you omit any of the three required arguments, the command will ask you to
+provide the missing values:
+
+  # command will ask you for the email
+  <info>php %command.full_name%</info> <comment>username password</comment>
+
+  # command will ask you for the email and password
+  <info>php %command.full_name%</info> <comment>username</comment>
+
+  # command will ask you for all arguments
+  <info>php %command.full_name%</info>
+
+HELP;
+    }
+}
